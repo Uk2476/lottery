7afbd6447e983aa5748e1eb7d010608b89a50aa9
Uk2476 <kuttkarsh028@gmail.com>
2026-03-01 10:34:51 +0530

    contract for rafflr

---
+++ src/Raffle.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+contract Raffle {
+
+    uint256  public enteranceFee;
+    uint256 public drawInterval ;
+
+    constructor (uint256 _fee , uint256 _interval) {
+        enteranceFee = _fee;
+        drawInterval = _interval;
+    }
+
+    function enterRaffle () public payable {
+        require(msg.value > _fee , insufficientEnterenceFee())
+    }
+
+    function pickWinner() 
+}
