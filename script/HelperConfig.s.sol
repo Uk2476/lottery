f3a05d92d570e233130c9d3e7f2e89c39f50a167
Uk2476 <kuttkarsh028@gmail.com>
2026-03-02 15:20:30 +0530

    helper file

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {Script, console2} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    
+}
