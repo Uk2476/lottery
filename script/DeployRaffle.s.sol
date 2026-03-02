89d834e09d8c23a9bb45c6dc32a72f38cb36fefa
Uk2476 <kuttkarsh028@gmail.com>
2026-03-02 16:04:11 +0530

    modified contract

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle){
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getCONFIG();
+
         Raffle raffle;
         vm.startBroadcast();
-        raffle = new Raffle();
+        raffle = new Raffle(
+            config.EnteranceFees,
+            config.DrawIntervals,
+            config.gasLane,
+            config.subscriptionId,
+            config.callBackGasLimit,
+            config.vrfCoordinatorV2_5
+        );
         vm.stopBroadcast();
     }
 }
