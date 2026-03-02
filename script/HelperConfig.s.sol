65f3f282bf3ce70a6b347b1a5cfc125da07ede98
Uk2476 <kuttkarsh028@gmail.com>
2026-03-02 15:32:33 +0530

    added struct

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,5 +5,12 @@ import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VR
 import {Script, console2} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
-    
+    struct NetworkConfig{
+        uint256 EnteranceFees;
+        uint256 DrawIntervals;
+        bytes32 gasLane;
+        uint256 subscriptionId;
+        uint32 callBackGasLimit;
+        address vrfCoordinatorV2_5;       
+    }
 }
