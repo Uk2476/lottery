4475608d638d0b98c60823c9892a45cc6ae2bebf
Uk2476 <kuttkarsh028@gmail.com>
2026-03-02 14:03:02 +0530

    fixedmore errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -4,83 +4,89 @@ pragma solidity ^0.8.18;
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract Raffle {
-
+contract Raffle is VRFConsumerBaseV2Plus {
     error notUpkeep();
     error insufficientEnterenceFee();
     error transactionFailed();
 
     bytes32 keyhash;
-    bytes extraArgs; 
+    bytes extraArgs;
     uint256 subId;
     uint16 public constant requestConfirmations = 5;
     uint32 callbackGasLimit;
-    uint32 numWords ;
+    uint32 constant numWords = 1;
     uint256 public enteranceFee;
-    uint256 public drawInterval ;
+    uint256 public drawInterval;
     uint256 lastDrawTimeStamp;
     uint256 requestId;
 
+    address payable[] private participantsAddresses;
+    address payable public raffleWinner;
 
-    address[] private payable participantsAddresses;
-    address public payable raffleWinner ;
-
-    constructor (uint256 _fee , uint256 _interval , bytes32 Keyhash , uint256 subscription , uint32 CallBackGasLimit , address vrfCoordinatorV2 ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+    constructor(
+        uint256 _fee,
+        uint256 _interval,
+        bytes32 Keyhash,
+        uint256 subscription,
+        uint32 CallBackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
         enteranceFee = _fee;
         drawInterval = _interval;
-        keyhash = Keyhash ;
+        keyhash = Keyhash;
         subId = subscription;
-        callbackGasLimit = CallBackGasLimit ;
-
+        callbackGasLimit = CallBackGasLimit;
     }
 
-    function enterRaffle () public payable {
-        require(msg.value > enteranceFee , insufficientEnterenceFee());
+    function enterRaffle() public payable {
+        if (msg.value < enteranceFee) {
+            revert insufficientEnterenceFee();
+        }
         participantsAddresses.push(payable(msg.sender));
     }
 
-    function checkUpkeep(bytes memory) public view returns ( bool Upkeep , bytes memory ){
-        bool interval = block.timestamp - lastDrawTimeStamp > drawInterval ; 
-        bool players = participantsAddresses.length > 0 ;
-        bool balance = address(this).balance > 0 ;
-        Upkeep = interval && players && balance ;
-        return (Upkeep , "" );
+    function checkUpkeep(bytes memory) public view returns (bool Upkeep, bytes memory) {
+        bool interval = block.timestamp - lastDrawTimeStamp > drawInterval;
+        bool players = participantsAddresses.length > 0;
+        bool balance = address(this).balance > 0;
+        Upkeep = interval && players && balance;
+        return (Upkeep, "");
     }
 
     function performUpkeep() external {
+        (bool check,) = checkUpkeep(bytes(""));
 
-        bool check = checkUpkeep("");
-
-        if (!check){
+        if (!check) {
             revert notUpkeep();
         }
 
-        
-        VRFV2PlusClient request = VRFV2PlusClient.RandomWordsRequest({
-            keyHash: keyHash,
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: keyhash,
             subId: subId,
             requestConfirmations: requestConfirmations,
             callbackGasLimit: callbackGasLimit,
             numWords: numWords,
-            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}));
-            })
-            
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        });
+
         requestId = s_vrfCoordinator.requestRandomWords(request);
 
         lastDrawTimeStamp = block.timestamp;
     }
 
-    function fulfillRandomWords(uint256 requestid, uint256[] memory randomWords) internal override{
-        uint256 noOfAccount = participantsAddress.length ;
-        uint256 indexSelected = randomWords[0]%noOfAccount ;
+    function fulfillRandomWords(uint256 _requestId, uint256[] calldata randomWords) internal override {
+        uint256 noOfAccount = participantsAddresses.length;
+        uint256 indexSelected = randomWords[0] % noOfAccount;
         raffleWinner = participantsAddresses[indexSelected];
-        (bool success,)= raffleWinner.call{value: address(this).balance}("");
-        require(success , transactionFailed());
+        (bool success,) = raffleWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert transactionFailed();
+        }
 
         lotteryRestart();
     }
 
     function lotteryRestart() internal {
-        participantsAddresses = new address payable [](0);
+        participantsAddresses = new address payable[](0);
     }
 }
