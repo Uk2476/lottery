9bc3198d2a4340e1037fe46f5e5ccbb94c283dcd
Uk2476 <kuttkarsh028@gmail.com>
2026-03-02 13:31:24 +0530

    fixed some errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -6,22 +6,26 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 
 contract Raffle {
 
+    error notUpkeep();
+    error insufficientEnterenceFee();
+    error transactionFailed();
+
     bytes32 keyhash;
     bytes extraArgs; 
     uint256 subId;
     uint16 public constant requestConfirmations = 5;
     uint32 callbackGasLimit;
     uint32 numWords ;
-    uint256  public enteranceFee;
+    uint256 public enteranceFee;
     uint256 public drawInterval ;
     uint256 lastDrawTimeStamp;
-    VRFV2PlusClient requestId;
+    uint256 requestId;
 
 
     address[] private payable participantsAddresses;
     address public payable raffleWinner ;
 
-    constructor (uint256 _fee , uint256 _interval , bytes32 Keyhash , uint256 subsciption , uint32 CallBackGasLimit , address vrfCoordinatorV2 ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+    constructor (uint256 _fee , uint256 _interval , bytes32 Keyhash , uint256 subscription , uint32 CallBackGasLimit , address vrfCoordinatorV2 ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
         enteranceFee = _fee;
         drawInterval = _interval;
         keyhash = Keyhash ;
@@ -31,31 +35,35 @@ contract Raffle {
     }
 
     function enterRaffle () public payable {
-        require(msg.value > _fee , insufficientEnterenceFee());
-        participantsAddress.push(payable(msg.sender));
+        require(msg.value > enteranceFee , insufficientEnterenceFee());
+        participantsAddresses.push(payable(msg.sender));
     }
 
-    function checkUpkeep(bytes memory) public returns ( bool Upkeep , bytes memory ){
+    function checkUpkeep(bytes memory) public view returns ( bool Upkeep , bytes memory ){
         bool interval = block.timestamp - lastDrawTimeStamp > drawInterval ; 
-        bool players = participantsAddress.length > 0 ;
+        bool players = participantsAddresses.length > 0 ;
         bool balance = address(this).balance > 0 ;
         Upkeep = interval && players && balance ;
-        return upkeep ;
+        return (Upkeep , "" );
     }
 
     function performUpkeep() external {
-        if (block.timestamp - lastDrawTimestamp < drawInterval){
-            revert intervalNotFinished();
+
+        bool check = checkUpkeep("");
+
+        if (!check){
+            revert notUpkeep();
         }
 
         
         VRFV2PlusClient request = VRFV2PlusClient.RandomWordsRequest({
-            keyHash: s_keyHash,
-            subId: s_subscriptionId,
+            keyHash: keyHash,
+            subId: subId,
             requestConfirmations: requestConfirmations,
             callbackGasLimit: callbackGasLimit,
             numWords: numWords,
-            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false});)})
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}));
+            })
             
         requestId = s_vrfCoordinator.requestRandomWords(request);
 
@@ -63,8 +71,8 @@ contract Raffle {
     }
 
     function fulfillRandomWords(uint256 requestid, uint256[] memory randomWords) internal override{
-        uint256 memory noOfAccount = participantsAddress.length ;
-        uint256 memory indexSelected = randomWords[0]%noOfAccount ;
+        uint256 noOfAccount = participantsAddress.length ;
+        uint256 indexSelected = randomWords[0]%noOfAccount ;
         raffleWinner = participantsAddresses[indexSelected];
         (bool success,)= raffleWinner.call{value: address(this).balance}("");
         require(success , transactionFailed());
